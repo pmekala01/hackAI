5610cc3f85eb87f2d7e9b44c14c68783210d23e7
amahag <56284413+amahag@users.noreply.github.com>
2023-04-15 21:21:22 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,26 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content.js']
-  });
+chrome.action.onClicked.addListener(async (tab) => {
+  const currentURL = tab.url;
+
+  if (currentURL.toLowerCase().endsWith('.pdf')) {
+    const response = await fetch(currentURL);
+    const data = await response.arrayBuffer();
+    const pdfData = new Uint8Array(data);
+    const loadingTask = pdfjsLib.getDocument({ data: pdfData });
+    const pdfDocument = await loadingTask.promise;
+
+    let extractedText = '';
+
+    for (let pageNumber = 1; pageNumber <= pdfDocument.numPages; pageNumber++) {
+      const page = await pdfDocument.getPage(pageNumber);
+      const textContent = await page.getTextContent();
+
+      extractedText += textContent.items.map(item => item.str).join(' ');
+    }
+
+    console.log('Extracted text:', extractedText);
+
+    // You can store or manipulate the extracted text here.
+  } else {
+    console.log('The current page is not a PDF file.');
+  }
 });
